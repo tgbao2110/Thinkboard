aa06ea5b152840976ff3f449642a0479f4fa18fd
tgbao2110 <tranb6778@gmail.com>
2025-06-20 14:00:36 +0700

    refactor server.js to connect DB before running

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,24 @@
-import express from 'express'
-import notesRoutes from './routes/notesRoutes.js'
-import { connectDB } from './config/db.js'
-import dotenv from 'dotenv'
-import rateLimiter from './middleware/rateLimiter.js'
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5001
-
-connectDB()
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(express.json()) // Middleware to parse JSON bodies
-app.use(rateLimiter)
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(rateLimiter);
 
 // Routes
-app.use("/api/notes", notesRoutes) // Use the notes routes for all requests to /api/notes
+app.use("/api/notes", notesRoutes); // Use the notes routes for all requests to /api/notes
 
-app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
-})
+// Connect DB and run
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+  });
+});
