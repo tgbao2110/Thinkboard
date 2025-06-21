141ce4a82a61f0ffb79e79ca1e5c6b2b4a61d2c1
tgbao2110 <tranb6778@gmail.com>
2025-06-21 17:01:45 +0700

    add CORS middleware, handle RateLimiting in frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -10,6 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter);
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import RateLimited from '../components/RateLimited';
+import axios from 'axios'
+import {toast} from 'react-hot-toast'
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = React.useState(false);
+  const [isRateLimited, setIsRateLimited] = React.useState(false)
+  const [notes, setNotes] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  //GET notes
+  React.useEffect(() => {
+    const fetchNotes = async() =>{
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes")
+        console.log(res.data)
+        setNotes(res.data)
+        setIsRateLimited(false)
+      } catch (error) {
+        if(error.response?.status == 429){
+          setIsRateLimited(true)
+        }
+        else{
+          toast.error(error.response?.message)
+        }
+      } finally{
+        setIsLoading(false)
+      }
+    };
+    fetchNotes();
+  }, [])
 
   return (
     <div className='min-h-screen'>
       <Navbar/>
       {isRateLimited && <RateLimited/>}
+      <div className='max-w-7xl mx-auto mt-6'>
+        {isLoading && <div className='text-primary text-center'>Loading notes...</div>}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {notes.map(note =>(
+            note
+            // <NoteCard key={note._id} note={note}/>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
