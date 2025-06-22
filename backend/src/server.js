44588a93ee19f7c84e8e4e3c1c7731e0a9e45334
tgbao2110 <tranb6778@gmail.com>
2025-06-22 14:25:23 +0700

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,44 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from "express"
+import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
-import rateLimiter from "./middleware/rateLimiter.js";
+import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js"
+import rateLimiter from "./middleware/rateLimiter.js"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 5001;
+const app = express()
+const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // Middleware
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use(rateLimiter);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  )
+  app.use(express.json()) // Middleware to parse JSON bodies
+  app.use(rateLimiter)
+}
+
 
 // Routes
-app.use("/api/notes", notesRoutes); // Use the notes routes for all requests to /api/notes
+app.use("/api/notes", notesRoutes) // Use the notes routes for all requests to /api/notes
+
+// Run the deployed product
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
+}
 
 // Connect DB and run
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-  });
-});
+    console.log("Server is running on port", PORT)
+  })
+})
